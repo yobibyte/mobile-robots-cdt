c9e283306b3417ec3d932f26d7f1a37e6aa5ab65
Ștefan Săftescu <stefan@robots.ox.ac.uk>
2018-03-02 18:45:04 +0000

    Refactor UndistortImage to make more readable.

--- sensors/UndistortImage.m
+++ sensors/UndistortImage.m
@@ -8,10 +8,13 @@ undistorted_image.cx = model.cx;
 undistorted_image.cy = model.cy;
 
 for k = 1:size(image.rgb, 3)
-    undistorted_image.rgb(:,:,k) = cast(reshape(interp2(cast(...
-        image.rgb(:,:,k), 'single'),  model.lut(:,1)+1,  model.lut(:,2)+1, ...
-        'bilinear'),  [size(image.rgb, 2), size(image.rgb, 1)]).', ...
-        'like', image.rgb);
+    image_s = cast(image.rgb(:, :, k), 'single');
+    undistorted_s = interp2(...
+      image_s, model.lut(:, 1) + 1, model.lut(:, 2) + 1, 'bilinear');
+    undistorted_s = reshape(...
+      undistorted_s, [size(image.rgb, 2), size(image.rgb, 1)]).';
+
+    undistorted_image.rgb(:, :, k) = cast(undistorted_s, 'like', image.rgb);
 end
 
 end
