76c7f734d409a01d0bce32eda11e4de88b3ee816
Alessandro De Palma <alessandrodepi@gmail.com>
2019-03-05 11:10:17 +0000

    working boolean finder (is the goal in the image?)

--- GoalFinder.m
+++ GoalFinder.m
@@ -1,8 +1,36 @@
 % TODO: goal finder, given the stereo images, return whether we see the goal and where it is x§§
 
-function [visible, target_pose] = GoalFinder(map, current_pose)
+function [visible, target_pose] = GoalFinder(images, current_pose)
 
   visible = false;
   target_pose = 'ciao';
+
+end
+
+
+function a = test_target_finder(image)
+
+  match_threshold = 1000;
+
+  n_colors = 10000;
+  map = hot(n_colors);
+  X = rgb2ind(image, map);
+
+  low = 900;
+  high = 3000;
+  RoI = roicolor(X,low,high);
+
+  n_ones = sum(RoI(:) == 1)
+
+  %figure(1)
+  %imshow(X, map)
+  %figure(2)
+  %imshow(RoI)
+
+  if n_ones > match_threshold
+    a = true
+  else
+    a = false
+  end
 
 end
