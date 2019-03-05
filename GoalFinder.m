5d87bf0055f26e1f42a4888a8f7bb4685ce31b47
Alessandro De Palma <alessandrodepi@gmail.com>
2019-03-05 09:49:59 +0000

    TitleCase all over the place

--- GoalFinder.m
+++ GoalFinder.m
@@ -1,6 +1,6 @@
 % TODO: goal finder, given the stereo images, return whether we see the goal and where it is x§§
 
-function [visible, target_pose] = goal_finder(map, current_pose)
+function [visible, target_pose] = GoalFinder(map, current_pose)
 
   visible = false;
   target_pose = 'ciao';
