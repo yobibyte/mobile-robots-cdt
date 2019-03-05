b68cbefba111570fe33ca489117713c70ff837c6
yobibyte <vitaliykurin@gmail.com>
2019-03-05 10:44:16 +0000

    merge

--- main.m
+++ main.m
@@ -17,36 +17,53 @@ config = GetHuskyConfig(husky_id);
 % Initialise mex-moos and register channels
 % clear mexmoos % not needed b/c matlab will check if it has already been
                 % initalised UNLESS you changed one of the five lines below
-client = ['ExampleCdtClient' num2str(int32(rand*1e7))];
-mexmoos('init', 'SERVERHOST', config.host, 'MOOSNAME', client);
-mexmoos('REGISTER', config.laser_channel, 0.0);
-mexmoos('REGISTER', config.stereo_channel, 0.0);
-mexmoos('REGISTER', config.wheel_odometry_channel, 0.0);
-pause(3); % Give mexmoos a chance to connect (important!)
+% client = ['ExampleCdtClient' num2str(int32(rand*1e7))];
+% mexmoos('init', 'SERVERHOST', config.host, 'MOOSNAME', client);
+% mexmoos('REGISTER', config.laser_channel, 0.0);
+% mexmoos('REGISTER', config.stereo_channel, 0.0);
+% mexmoos('REGISTER', config.wheel_odometry_channel, 0.0);
+% pause(3); % Give mexmoos a chance to connect (important!)
 
+filename = '2019-03-04-17-27-25.mat';
+collected_data = load(filename);
+scans = collected_data.scans;
+odometries = collected_data.odometries;
+images = collected_data.images;
+ITERS = size(scans, 2);
 
-P = eye(1); % initialise covariance matrix somehow
-x = zeros(1, 1); % init the state vector
-% Main loop
-while true
-    % Fetch latest messages from mex-moos
-    mailbox = mexmoos('FETCH');
-    scan = GetLaserScans(mailbox, config.laser_channel, true);
-    stereo_images = GetStereoImages(mailbox, config.stereo_channel, true);
-    wheel_odometry = GetWheelOdometry(mailbox, ...
-                                      config.wheel_odometry_channel, ...
-                                      true);
+P = eye(1); % initialise covariance matrix
+x = zeros(1, 3); % init the state vector, first three coords are our pose
 
-
+for s = 1:ITERS
+    scan = scans{s};
     poles = PoleDetector(scan, 800);
-    [x_new, P] = SLAMUpdate(wheel_odometry, poles, x, P);
-
-    current_pose = x_new(1, :); % TODO
-    map = x_new(2:end, :); % TODO
+    wheel_odometry = odometries{s};
+    %[x_new, P] = SLAMUpdate(wheel_odometry, poles, x, P);
+    %current_pose = x_new(1, :); % TODO
+    %map = x_new(2:end, :); % TODO
     %TODO: add check on goal detection somewhere
-    target_pose = RoutePlanner(map, current_pose); % TODO.
-
-    velocity, angle = WheelController(current_pose, target_pose);
-    SendSpeedCommand(velocity, angle, husky_config.control_channel);
-
+    %target_pose = route_planner(map, current_pose); % TODO.
+    %velocity, angle = wheel_controller(current_pose, target_pose);
+    %SendSpeedCommand(velocity, angle, husky_config.control_channel);
 end
+
+% while true
+    % Fetch latest messages from mex-moos
+    %mailbox = mexmoos('FETCH');
+    %scan = GetLaserScans(mailbox, config.laser_channel, true);
+    %stereo_images = GetStereoImages(mailbox, config.stereo_channel, true);
+    %wheel_odometry = GetWheelOdometry(mailbox, ...
+                                      %config.wheel_odometry_channel, ...
+                                      %true);
+
+    %poles = PoleDetector(scan, 800);
+    %[x_new, P] = SLAMUpdate(wheel_odometry, poles, x, P);
+
+    %current_pose = x_new(1, :); % TODO
+    %map = x_new(2:end, :); % TODO
+    %target_pose = route_planner(map, current_pose); % TODO.
+
+    %velocity, angle = wheel_controller(current_pose, target_pose);
+    %SendSpeedCommand(velocity, angle, husky_config.control_channel);
+
+% end
