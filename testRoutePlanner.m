67b99916163ae663a1587dd781a7f1b52f9bc61e
Panagiotis Tigas <ptigas@gmail.com>
2019-03-05 20:47:32 +0000

    use planner from matlab

--- testRoutePlanner.m
+++ testRoutePlanner.m
@@ -1,9 +1,11 @@
 clear;
-hold off;
+hold on;
 map = cell(10, 2);
 for i=1:10
-   map{i} = [random('Normal',0,50) random('Normal',0,50)]; 
+   map{i} = [random('Normal',0,5) random('Normal',0,5)]; 
 end
 map = cell2mat(map);
-
-RoutePlanner(map, [0 -100 0], [0 100 0])
+start_pose = [random('Normal',0,5) random('Normal',0,5) 0]
+end_pose = [random('Normal',0,5) random('Normal',0,5) 0]
+target = RoutePlanner(map,  start_pose, end_pose)
+scatter(target(1), target(2));
