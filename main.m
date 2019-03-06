d5f65c837b53696f57c2f63de5e007a13a9421a3
yobibyte <vitaliykurin@gmail.com>
2019-03-06 14:38:51 +0000

    merge

--- main.m
+++ main.m
@@ -24,24 +24,38 @@ config = GetHuskyConfig(husky_id);
 % mexmoos('REGISTER', config.wheel_odometry_channel, 0.0);
 % pause(3); % Give mexmoos a chance to connect (important!)
 
-filename = '2019-03-04-17-27-25.mat';
-%filename = '2019-03-06-10-47-35.mat';
-collected_data = load(filename);
-scans = collected_data.scans;
-odometries = collected_data.odometries;
-images = collected_data.images;
-ITERS = size(scans, 2);
+MODE = 1; % 0 for real, 1 for replay, 2 for fake data
+ITER = intmax;
 
 P = eye(3); % initialise covariance matrix
 x = zeros(3, 1); % init the state vector, first three coords are our pose
 
 
+if MODE == 1
+    filename = '2019-03-06-12-12-27.mat';
+    %filename = '2019-03-06-10-47-35.mat';
+    collected_data = load(filename);
+    scans = collected_data.scans;
+    odometries = collected_data.odometries;
+    images = collected_data.images;
+    ITERS = size(scans, 2);
+end
+
 for s = 1:ITERS
-    scan = scans{s};
+    if MODE == 1
+        scan = scans{s};
+        od = odometries{s};
+    else
+        mailbox = mexmoos('FETCH');
+        scan = GetLaserScans(mailbox, config.laser_channel, true);
+        stereo_images = GetStereoImages(mailbox, config.stereo_channel, true);
+        
+        od = GetWheelOdometry(mailbox, config.wheel_odometry_channel);
+    end
+    
     poles = PoleDetector(scan, 700);
     poles = reshape(cell2mat(poles), [], 2)';
     
-    od = odometries{s};
     ssize = size(od, 2);
     
     odx = 0;
@@ -57,14 +71,25 @@ for s = 1:ITERS
     
     u = [odx; ody; odyaw];
     [x, P] = SLAMUpdate(u, poles, x, P);
-    
     map = reshape(x(4:end), [], 2);
+    
+    goal_reached = false;
+    % goal_reached = ...; % TODO goal reached check
+    if goal_reached
+        break
+    end
+      
+    
+    % target_pose = route_planner(map, x(1:3)); % TODO.
+    % velocity, angle = wheel_controller(current_pose, target_pose);
+    % SendSpeedCommand(velocity, angle, husky_config.control_channel);
+
     plot_state(x(1:3), map, poles, images{s}.left.rgb, s);
     
     pause(0.4);
 end
-% TODO sync wheelodpometry frequency sampling
-% TODO 
+
+
 function plot_state(robot_pose, map, poles, image, iter)
     clf();
     subplot(1, 2, 1);
@@ -77,7 +102,7 @@ function plot_state(robot_pose, map, poles, image, iter)
     l = robot_y - k*robot_x;
     xprime = robot_x+0.5;
     yprime = k * (xprime) + l;
-
+    
     hold on;
 
     % plot the slam state
@@ -87,8 +112,7 @@ function plot_state(robot_pose, map, poles, image, iter)
     % plot the tobot
     scatter(robot_x, robot_y, 'red');
     plot([robot_x, xprime],[robot_y, yprime], 'red');
-
-    % plot poles
+    
     [px, py] = pol2cart(poles(2, :)' + robot_yaw, poles(1, :)');
     scatter(px + robot_x, py + robot_y, 'magenta');
 
@@ -101,22 +125,3 @@ function plot_state(robot_pose, map, poles, image, iter)
     imshow(image)
     title(num2str(iter));
 end
-% while true
-    % Fetch latest messages from mex-moos
-    %mailbox = mexmoos('FETCH');
-    %scan = GetLaserScans(mailbox, config.laser_channel, true);
-    %stereo_images = GetStereoImages(mailbox, config.stereo_channel, true);
-    %wheel_odometry = GetWheelOdometry(mailbox, ...
-                                      %config.wheel_odometry_channel, ...
-                                      %true);
-    %poles = PoleDetector(scan, 800);
-    %[x_new, P] = SLAMUpdate(wheel_odometry, poles, x, P);
-
-    %current_pose = x_new(1, :); % TODO
-    %map = x_new(2:end, :); % TODO
-    %target_pose = route_planner(map, current_pose); % TODO.
-
-    %velocity, angle = wheel_controller(current_pose, target_pose);
-    %SendSpeedCommand(velocity, angle, husky_config.control_channel);
-
-% end
