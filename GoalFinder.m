3d841d7c0a3403ce15ce2ec8451ab6b05519ddb3
Alessandro De Palma <alessandrodepi@gmail.com>
2019-03-06 10:45:28 +0000

    commit nonworking goalfinder

--- GoalFinder.m
+++ GoalFinder.m
@@ -2,7 +2,30 @@
 
 function [visible, target_pose] = GoalFinder(images, current_pose)
 
-  visible = false;
+  husky_id = 2; % Modify for your Husky
+
+  % Get the channel names and sensor IDs for this Husky
+  config = GetHuskyConfig(husky_id);
+
+  images = UndistortStereoImage(images, config.camera_model);
+  [left_is_goal, left_goal_coordinates] = target_finder(images.left.rgb);
+  [right_is_goal, right_goal_coordinates] = target_finder(images.right.rgb);
+  visible = left_is_goal && right_is_goal;
+  if not(visible)
+    return
+  end
+
+  % Camera parameters definition.
+  K = [config.camera_model.left.fx 0 0; 0 config.camera_model.left.fy 0; config.camera_model.left.cx config.camera_model.left.cy 1];
+  P = K * [eye(3) zeros(3, 1)];
+
+  stereo_rotation = eye(3);
+  stereo_translation = [config.camera_model.baseline; 0; 0];
+  P_prime = K * [stereo_rotation stereo_translation];
+
+  A = [left_goal_coordinates(1) * P(3, :) - P(1, :); left_goal_coordinates(2) * P(3, :) - P(2, :); right_goal_coordinates(1) * P_prime(3, :) - P_prime(1, :); right_goal_coordinates(2) * P_prime(3, :) - P_prime(2, :)];
+  reconstruction = null(A);
+
   target_pose = 'ciao';
 
 end
@@ -23,7 +46,7 @@ function [is_goal, goal_coordinates] = target_finder(image)
   high = 4000;
   RoI = roicolor(X,low,high);
 
-  n_ones = sum(RoI(:) == 1)
+  n_ones = sum(RoI(:) == 1);
 
   %figure(1)
   %imshow(X, map)
@@ -40,7 +63,7 @@ function [is_goal, goal_coordinates] = target_finder(image)
   end
   k=2; %Number of classes
   [idx,centroid] = kmeans(cluster_data,k);
-  c_dist = pdist(centroid,'euclidean')
+  c_dist = pdist(centroid,'euclidean');
   if c_dist < 130
      goal_coordinates = (centroid(1, :) + centroid(2, :))/2;
   else
@@ -53,14 +76,54 @@ function [is_goal, goal_coordinates] = target_finder(image)
     end
   end
 
+  goal_coordinates = round(goal_coordinates);
+
   %marked = insertMarker(X,goal_coordinates,'x', 'size',10);
   %figure(3)
   %imshow(marked, map)
 
   if n_ones > match_threshold
-    is_goal = true
+    is_goal = true;
   else
-    is_goal = false
+    is_goal = false;
   end
 
 end
+
+
+
+
+
+
+
+
+
+
+% Camera parameters definition.
+%intrinsic_matrix = [config.camera_model.left.fx, 0, 0; 0, config.camera_model.left.fy, 0; config.camera_model.left.cx, config.camera_model.left.cy, 1];
+%cameraParams = cameraParameters('IntrinsicMatrix', intrinsic_matrix);
+
+%stereo_rotation = eye(3);
+%stereo_translation = [config.camera_model.baseline, 0, 0];
+%stereoParams = stereoParameters(cameraParams, cameraParams, stereo_rotation, stereo_translation);
+
+
+%[frameLeftRect, frameRightRect] = rectifyStereoImages(images.left.rgb, images.right.rgb, stereoParams);
+%frameLeftGray  = rgb2gray(frameLeftRect);
+%frameRightGray = rgb2gray(frameRightRect);
+
+%disparityMap = disparity(frameLeftGray, frameRightGray);
+%figure;
+%imshow(disparityMap, [0, 64]);
+%title('Disparity Map');
+%colormap jet
+%colorbar
+
+%points3D = reconstructScene(disparityMap, stereoParams)
+
+%points3D = points3D ./ 1000;  % convert to meters
+
+%left_goal_coordinates
+%points3D(left_goal_coordinates)
+% Find the distances from the camera in meters.
+%dists = sqrt(sum(points3D(left_goal_coordinates) .^ 2))
