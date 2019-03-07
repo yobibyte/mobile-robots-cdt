1edcad32a412f0f9098103378ef5787f0fdd2919
Panagiotis Tigas <ptigas@gmail.com>
2019-03-07 15:04:24 +0000

    tested controller

--- control/WheelController.m
+++ control/WheelController.m
@@ -1,26 +1,38 @@
 classdef WheelController
    properties
-      a_pid = PID(0.06, 0, 0.008)
-      v_pid = PID(0.06, 0, 0.008)
-      lx_pid = PID(0.06, 0, 0.008)
-      ly_pid = PID(0.06, 0, 0.008)
+      a_pid = PID(0.8, 0, 0.0)
+      v_pid = PID(0.5, 0, 0.0)
+      lx_pid = PID(0.06, 0, 0.0)
+      ly_pid = PID(0.06, 0, 0.0)
    end
    methods
-      function [distance, angular_velocity, linear_velocity] = update(obj, current_pose, target_pose)          
-          distance = pdist([current_pose(1), current_pose(2); target_pose(1) target_pose(2)])
+      function [distance, angular_velocity, linear_velocity, velocity] = update(obj, current_pose, target_pose)    ;      
+          distance = pdist([current_pose(1), current_pose(2); target_pose(1) target_pose(2)]);
           theta = atan2(target_pose(2)-current_pose(2), target_pose(1)-current_pose(1));
-          linear_velocity = [0 0]
-          if distance > 0.1
-              angular_velocity = obj.a_pid.update(current_pose(3), theta);              
-              if (abs(current_pose(3) - theta) < 0.1)
-                velocity = -obj.v_pid.update(distance, 0);
-                linear_velocity = [cos(theta) sin(theta)]*velocity;
-              end
-          else
-              angular_velocity = obj.a_pid.update(current_pose(3), target_pose(3));       
-              velocity = -obj.v_pid.update(distance, 0);
-              linear_velocity = [cos(theta) sin(theta)]*velocity;
-          end          
+          linear_velocity = [0 0];
+          velocity = 0;
+          angular_velocity = obj.a_pid.update(current_pose(3), theta);              
+          velocity = -obj.v_pid.update(distance, 0);
+          linear_velocity = [cos(theta) sin(theta)]*velocity;          
+          
+          if distance < 0.04              
+              % rotate in place
+              angular_velocity = obj.a_pid.update(current_pose(3), target_pose(3));              
+              velocity = 0;
+              linear_velocity = [cos(theta) sin(theta)]*velocity;          
+          end
+
+%           if distance > 0.1
+%               angular_velocity = obj.a_pid.update(current_pose(3), theta);              
+%               if (abs(current_pose(3) - theta) < 0.1)
+%                 velocity = -obj.v_pid.update(distance, 0);
+%                 linear_velocity = [cos(theta) sin(theta)]*velocity;
+%               end
+%           else
+%               angular_velocity = obj.a_pid.update(current_pose(3), target_pose(3));       
+%               velocity = -obj.v_pid.update(distance, 0);
+%               linear_velocity = [cos(theta) sin(theta)]*velocity;
+%           end          
       end
    end
 end
--- control/testWheelController.m
+++ control/testWheelController.m
@@ -1,21 +1,63 @@
+husky_id = 2; % Modify for your Husky
 
-clear all;
-s = [0 0 pi/180.0*(45+90)];
-e = [10 10 pi/180.0*(-90)];
+% Get the channel names and sensor IDs for this Husky
+config = GetHuskyConfig(husky_id);
+
+% Initialise mex-moos and register channels
+clear mexmoos % not needed b/c matlab will check if it has already been
+                % initalised UNLESS you changed one of the five lines below
+client = ['ExampleCdtClient' num2str(int32(rand*1e7))];
+mexmoos('init', 'SERVERHOST', config.host, 'MOOSNAME', client);
+mexmoos('REGISTER', config.laser_channel, 0.0);
+mexmoos('REGISTER', config.stereo_channel, 0.0);
+mexmoos('REGISTER', config.wheel_odometry_channel, 0.0);
+pause(3); % Give mexmoos a chance to connect (important!)
+SendSpeedCommand(0, 0, config.control_channel)
+
+s = [0 0 deg2rad(0)];
+e = [3 2 deg2rad(180)];
 a = s
 controller = WheelController
+SendSpeedCommand(0, 0, config.control_channel);
+
+cla
+hold on
+disp(a);
+scatter(e(1), e(2), 'x');
 while true
-    cla
-    hold on
-    disp(a);
-    PlotPose(a);
-    PlotPose(s);    
-    PlotPose(e);
-
-    [distance, angular_velocity, linear_velocity] = controller.update(a, e);
-
-    a(3) = a(3) + angular_velocity;
-    a(1:2) = a(1:2) + linear_velocity;
     
-    pause(0.1); % don't overload moos w/commands
+    %PlotPose(a);
+    %PlotPose(s);    
+    %PlotPose(e);
+    
+    mailbox = mexmoos('FETCH');
+    od = GetWheelOdometry(mailbox, config.wheel_odometry_channel, false);
+    
+    % update s
+    G_last = BuildSE2Transform(a);
+    for idx = 1:size(od, 2)
+        G_last_current = BuildSE2Transform([od(idx).x,od(idx).y, od(idx).yaw]);
+        G_last = G_last * G_last_current;
+    end
+    
+    a = SE2ToComponents(G_last);
+    %a = a + u;
+    disp(a);
+    
+    [distance, angular_velocity, linear_velocity, velocity] = controller.update(a, e);
+    velocity = min(velocity, 0.5);
+    angular_velocity = min(angular_velocity, 0.5);
+    fprintf("Angular velocity %f\n", angular_velocity);
+    SendSpeedCommand(velocity, angular_velocity, config.control_channel);
+    
+    if (angular_velocity ~= 0)
+        
+    end
+    
+    scatter(a(1), a(2));
+    
+    %a(3) = a(3) + angular_velocity;
+    %a(1:2) = a(1:2) + linear_velocity;
+    
+    pause(0.01); % don't overload moos w/commands
 end
