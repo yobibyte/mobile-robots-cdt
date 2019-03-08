97fac0e9e5bb815db57f27752b309ff2224d72f0
Panagiotis Tigas <ptigas@gmail.com>
2019-03-08 09:41:58 +0000

    route planner changes

--- RoutePlanner.m
+++ RoutePlanner.m
@@ -2,7 +2,7 @@
 
 function [prm, path] = RoutePlanner(poles, current_pose, target)
 
-  rng(42)
+  rng(42);
 
   data = [[current_pose(1), current_pose(2)]; poles; [target(1), target(2)]];
   min_ = min(data)-[1, 1];
@@ -15,7 +15,7 @@ function [prm, path] = RoutePlanner(poles, current_pose, target)
   target(1) = target(1)-min_(1);
   target(2) = target(2)-min_(2);
 
-  map = robotics.BinaryOccupancyGrid(ceil(max_(1)-min_(1)),ceil(max_(2)-min_(2)), 100);
+  map = robotics.BinaryOccupancyGrid(ceil(max_(1)-min_(1)),ceil(max_(2)-min_(2)), 10);
   for i=1:size(poles, 1)
       setOccupancy(map,[poles(i, 1) poles(i, 2)], 1)
   end
@@ -23,8 +23,8 @@ function [prm, path] = RoutePlanner(poles, current_pose, target)
   inflate(map, 0.5);
   prm = robotics.PRM;
   prm.Map = map;
-  prm.NumNodes = 50;
-  prm.ConnectionDistance = 5;
+  prm.NumNodes = 150;
+  prm.ConnectionDistance = 15;
   %if (getOccupancy(map, [target(1), target(2)]))
   try
     path = findpath(prm, [current_pose(1), current_pose(2)], [target(1), target(2)]);
